a305cf4d1f7e8470a5f51cbbd61530d28a5b6649
winfredselwyn <winfredselwyn@gmail.com>
2017-07-01 08:00:10 +0800

    Added Day-04

--- day-04.js
+++ day-04.js
@@ -1,15 +1,18 @@
 function Person(initialAge){
     // Add some more code to run some checks on initialAge
-   let age = initialAge;
-   if (age < 0) {
-       process.stdout.write('Age is not valid, setting age to 0');
+   if (initialAge < 0) {
+       process.stdout.write('Age is not valid, setting age to 0.' + '\n');
+       initialAge = 0;
    };
    
    this.amIOld=function(){
         // Do some computations in here and print out the correct statement to the console
-
+        if (initialAge < 13) process.stdout.write('You are young.' + '\n');
+        if (initialAge < 18 && initialAge >= 13) process.stdout.write('You are a teenager.' + '\n');
+        if (initialAge >= 18) process.stdout.write('You are old.' + '\n');
    };
    this.yearPasses=function(){
           // Increment the age of the person in here
+        ++initialAge
    };
 }
