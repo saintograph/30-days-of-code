4abc3155247ae40c05674360e2b0205bbcdea691
winfredselwyn <winfredselwyn@gmail.com>
2017-07-06 13:29:44 +0800

    Added Day-08.

--- day-08.js
+++ day-08.js
@@ -1,27 +1,22 @@
 function processData(input) {
-    // Enter your code here
-    // process.stdout.write(input.charAt(0))
-    
-    const arrayKeyValues = input.split('\n');
-    const newArray = String(arrayKeyValues).split(' ');
-    const arrayThree = String(newArray).split(',');
-    //process.stdout.write(String(arrayThree));
-    const foo = new Object();
-    //let counter = Number(input.charAt(0));
-    const counter = Number(input.charAt(0)) + 2;
-    while(counter > 1) {
-        foo[arrayThree[counter]] = arrayThree[counter];
-        counter - 2;
-        //process.stdout.write(String(bar));
+    input = input.split('\n')
+    var N = parseInt(input[0])
+
+    var phonebook = []
+    for (var k = 0; k< N; k++){
+        var line = input[k+1]
+        line = line.split(' ')
+        phonebook[line[0]] = line[1]
     }
-    //process.stdout.write(String(foo))
-    process.stdout.write(JSON.stringify(foo))
-    //for (let i; i != 0; --i) {
-     //    foo.[i] = function () {
-             
-             
-         //}
-    //}
+
+    for (var k = N+1; k < input.length; k++ ){
+        var num = (phonebook[input[k]])
+        if (num !== undefined){
+            console.log(input[k]+'='+num)
+        } else {
+            console.log('Not found')
+        }
+    }
 } 
 
 process.stdin.resume();
